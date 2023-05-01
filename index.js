d844ca295d6011d7a1fe3ec5395ad4952615dbf6
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-05-01 02:13:46 -0300

    feat: add more features to linked list

--- index.js
+++ index.js
@@ -38,18 +38,42 @@ export default class LinkedList {
             currentNode = currentNode.next;
             counter++;
         }
-
     }
 
     remove(element) {
 
     }
 
-    indexOf(element) {
-
+    removeAt(index) {
+        console.log('inside remove at, index = ', index);
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        if (this.size() === 1) {
+            this.head = null;
+            this.count = 0;
+        } else if (index + 1 > this.size()) {
+            return undefined;
+        } else {
+            let currentNode = this.head;
+            let counter = 0;
+            let prevNode = null;
+            while (currentNode != null) {
+                console.log({ counter });
+                if (counter === index) {
+                    const deletedNode = currentNode;
+                    prevNode.next = currentNode.next;
+                    this.count--;
+                    return deletedNode;
+                }
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+                counter++;
+            }
+        }
     }
 
-    removeAt(position) {
+    indexOf(element) {
 
     }
 
@@ -66,7 +90,6 @@ export default class LinkedList {
     }
 
     printThis() {
-        console.log(this.head);
         console.log(this);
     }
 
@@ -78,71 +101,15 @@ linkedList.push(2);
 linkedList.push(4);
 linkedList.push(1);
 linkedList.printThis();
+const removedAt1 = linkedList.removeAt(1);
+console.log({ removed: removedAt1 });
+linkedList.printThis();
 const currentItemIndex1 = linkedList.getElementAt(1);
 const currentItemIndex2 = linkedList.getElementAt(2);
 const currentItemIndex0 = linkedList.getElementAt(0);
 const currentItemIndex4 = linkedList.getElementAt(4);
+linkedList.printThis();
 console.log({ currentItemIndex1 });
 console.log({ currentItemIndex2 });
 console.log({ currentItemIndex0 });
 console.log({ currentItemIndex4 });
-
-class LinkedList2 {
-    constructor(equalsFn = defaultEquals) {
-        this.count = 0;
-        this.head = undefined;
-        this.equalsFn = equalsFn;
-    }
-
-    push(element) {
-        const node = new Node(element);
-        if (this.head == null) {
-            this.head = node;
-        } else {
-            let currentNode = this.head;
-            while (currentNode.next != null) {
-                currentNode = currentNode.next;
-            }
-            currentNode.next = node;
-        }
-        this.count++;
-    }
-
-    insert(element, position) {
-
-    }
-
-    getElementAt(index) {
-
-    }
-
-    remove(element) {
-
-    }
-
-    indexOf(element) {
-
-    }
-
-    removeAt(position) {
-
-    }
-
-    isEmpty() {
-        return this.count === 0;
-    }
-
-    size() {
-        return this.count;
-    }
-
-    toString() {
-
-    }
-
-    printThis() {
-        console.log(this.head);
-        console.log(this);
-    }
-
-}
